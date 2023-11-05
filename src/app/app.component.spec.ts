c19ac24bda6f6be2de08138f83f3013f7ee3c317
izmjen <janjira.kha@ku.th>
2023-11-05 21:50:01 +0700

    fix the errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crud-app'`, () => {
+  it(`should have as title 'CrudApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud-app');
+    expect(app.title).toEqual('CrudApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crud-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CrudApp app is running!');
   });
 });
